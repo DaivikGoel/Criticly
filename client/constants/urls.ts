bdbf0ed27fbe3305a424f9340dfb7f030db5f1bf
Yuvraj <yuvrajsinghhundal@gmail.com>
2021-03-20 13:40:31 -0600

    CK-22 more minor styling to profile page

--- client/constants/urls.ts
+++ client/constants/urls.ts
@@ -4,3 +4,4 @@ export const search_url_people = 'https://api.themoviedb.org/3/search/person?api
 export const trending_url ='https://api.themoviedb.org/3/trending/tv/day?api_key=';
 export const topRated_url = 'https://api.themoviedb.org/3/tv/top_rated?api_key=';
 export const popular_url = 'https://api.themoviedb.org/3/tv/popular?api_key=';
+export const test_image = 'https://raw.githubusercontent.com/AboutReact/sampleresource/master/old_logo.png';
--- client/screens/ProfileScreen.tsx
+++ client/screens/ProfileScreen.tsx
@@ -1,21 +1,28 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
-
-import List from '../components/List';
+import { StyleSheet, Image } from 'react-native';
 import { Text, View } from '../components/Themed';
+import {test_image} from '../constants/urls';
 
 export default function ProfileScreen() {
   return (
     <View style={styles.container}>
+      <Image 
+        source={{ uri: test_image}} 
+        style={styles.displayPic} 
+      />
       <Text style={styles.header}>Username </Text>
       <Text style={styles.bio}> This will be the bio text, put some limit of like 300 characters or something</Text>
       <Text style={styles.header}>My favourites</Text>
-      <Text style={styles.header}>Activity</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  displayPic: {
+    width: 100, 
+    height: 100, 
+    borderRadius: 400/ 2
+  },
   header:{
     paddingTop:'15%',
     fontSize:30
@@ -26,6 +33,9 @@ const styles = StyleSheet.create({
   },
   container:{
     backgroundColor:'purple',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
     width:'100%',
     height:'100%'
   }
