65bd9bd4cd1f87ccbb5b3c6f4e95451f924bf27c
Daivik Goel <daivikgoel98@gmail.com>
2021-03-27 19:50:54 -0400

    Ck-43 Fixed small syntax error

--- server/routes/getreviews.js
+++ server/routes/getreviews.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 router.get('/', function(req, res, next) {
     switch (req.query.type){
       case ('latest'):
-        var sql = "SELECT reviews.*, users.username FROM reviews INNER JOIN users ON (reviews.userid = users.id) WHERE episodeid = " + req.query.episodeid + "ORDER BY modified_instant LIMIT 1"
+        var sql = "SELECT reviews.*, users.username FROM reviews INNER JOIN users ON (reviews.userid = users.id) WHERE episodeid = " + req.query.episodeid + " ORDER BY modified_instant LIMIT 1"
         con.query(sql, function (err, result) {
           if (err) throw err;
           console.log(result)
