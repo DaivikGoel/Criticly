436117e2780bfe78c2d2ee9a0904cb80b105a022
Yuvraj <yuvrajsinghhundal@gmail.com>
2021-03-27 15:33:26 -0600

    resolve merge conflict

--- server/app.js
+++ server/app.js
@@ -7,7 +7,12 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var postReviewsRouter = require('./routes/postreview');
+<<<<<<< Updated upstream
 var getReviewsRouter = require('./routes/getreviews');
+=======
+var getAggregateReviewsRouter = require('./routes/aggregateReview');
+
+>>>>>>> Stashed changes
 var app = express();
 
 // view engine setup
@@ -23,8 +28,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/postreview', postReviewsRouter);
+<<<<<<< Updated upstream
 app.use('/getreviews', getReviewsRouter);
 
+=======
+app.use('/aggregateReviews', getAggregateReviewsRouter);
+>>>>>>> Stashed changes
 
 const con = require('./config.js')
 con.connect(function(err) {
