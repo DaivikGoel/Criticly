e18065e845787603d10c0c222a03c816e6278e12
Yuvraj <yuvrajsinghhundal@gmail.com>
2021-04-02 16:50:38 -0600

    CK-65 add initial test for postreviewlike

--- server/util/sqlWrapper.js
+++ server/util/sqlWrapper.js
@@ -1,8 +1,12 @@
 const con = require('../config.js')
-const executeQuery = (sql,req,res) => 
-    con.query(sql, function (err, result) {
-        if (err) 
-            throw err;
-        res.send(result);
+    const executeQuery = (sql,req,res) => {
+        con.query(sql, function (err, result) 
+        {   
+            if (err) {
+                res.status(404).json({"status": "failure", "message": 'error occured in deleting: code - ' + err.code + " ,isFatal - " + err.fatal });
+              }
+              res.send(result);
         })
+    }
+
 module.exports = executeQuery;
