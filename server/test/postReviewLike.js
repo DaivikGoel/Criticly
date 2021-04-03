fb97de34afb632e0507c376093a7c5b085f48b41
Yuvraj <yuvrajsinghhundal@gmail.com>
2021-04-02 18:35:45 -0600

    CK-65 fix test file names

--- server/test/postReviewLike.js
+++ server/test/postReviewLike.js
@@ -1,78 +1,22 @@
-const {expect} = require('chai');
+const {expect, should} = require('chai');
 const request = require('supertest');
+var assert = require('assert');
 
 const app = require('../app.js');
 const { strictEqual } = require('assert');
 
-const testUserId = 9;
-const searchTypeUser = 'user';
-const searchTypeLatest ='latest';
-const Http200Reponse = 200;
-const testUserShowId = 88396;
-const testEpisodenumber = 1;
-const testSeasonnumber = 1;
-
-const sqlQueryResponseForTestUser = 
-    
-    [{
-        "reviewid":25,
-        "userid":9,
-        "episodenumber":1,
-        "seasonnumber":1,
-        "showid":88396,
-        "rating":5,
-        "reviewtext":"NEW WORLD ORDER \n\n\n\n\nI LOVBE MARVEL\n\n\nWOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
-        "created_instant":"2021-04-02T08:54:06.000Z",
-        "modified_instant":"2021-04-02T08:54:06.000Z"
-    }]
-
-const sqlQueryResponseForTestUserLatest =
-[
-    {
-      reviewid: 20,
-      userid: 10,
-      episodenumber: 1,
-      seasonnumber: 1,
-      showid: 88396,
-      rating: 3,
-      reviewtext: 'Wasn’t a fan',
-      created_instant: '2021-03-31T02:30:35.000Z',
-      modified_instant: '2021-03-31T02:30:35.000Z',
-      username: 'Bojiizle',
-      numberofLikes: 2,
-      numberofComments: 1,
-      hasUserLiked: 1
-    }
-  ]
-    
+const exsistingUserId = 88890134;
+const existingReviewId = 8889013;
+const Http404Reponse = 404;
 
 
-describe('GET /getreviews', () => {
-    it('Get a list of reviews for by user', (done) => {
-        request(app).get('/getreviews?userid=' + testUserId + '&type=' +searchTypeUser)
+
+describe('POST /reviewlike', () => {
+    it('expect 404 response when trying to insert existing record', (done) => {
+        request(app).post('/getreviews')
+        .send({type:'add',reviewid: exsistingUserId, existingReviewId: existingReviewId})
         .then((res) =>{
-            strictEqual(res['statusCode'], Http200Reponse);
-            let sqlResponse = JSON.parse(res['text']);
-            console.log(sqlResponse)
-            expect(sqlQueryResponseForTestUser).to.be.deep.equal(sqlResponse)
+            strictEqual(res['statusCode'], Http404Reponse);
         }).then(done, done);
     })
 });
-
-describe('GET /getreviews', () => {
-    it('Get', (done) => {
-        request(app).get('/getreviews?episodenumber=' + testEpisodenumber + 
-                            '&seasonnumber=' +testSeasonnumber +
-                            '&showid=' + testUserShowId +
-                            '&userid=' + testUserId +
-                            '&type=' + searchTypeLatest)
-        .then((res) =>{
-            strictEqual(res['statusCode'], Http200Reponse);
-            let sqlResponse = JSON.parse(res['text']);
-            console.log(sqlResponse)
-            expect(sqlQueryResponseForTestUserLatest).to.be.deep.equal(sqlResponse)
-        }).then(done, done);
-    })
-});
-
-
